07940b64383794242259e611f208761a40c886e3
PCQ59 <shreyash.zala@tatvasoft.com>
2025-07-28 17:10:14 +0530

    Update Appspec File

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-ECR_URI="979566283645.dkr.ecr.ap-south-1.amazonaws.com/simple-node-app"
-IMAGE_TAG="latest"
-CONTAINER_NAME="simple-node-app"
 
-# Stop & remove existing container
+# Define variables
+AWS_REGION=ap-south-1
+ECR_URI=979566283645.dkr.ecr.$AWS_REGION.amazonaws.com
+CONTAINER_NAME=simple-node-app
+IMAGE_TAG=latest
+REPO_URI=979566283645.dkr.ecr.$AWS_REGION.amazonaws.com/simple-node-app
+
+# Stop and remove old container if exists
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
-# Pull latest image
-aws ecr get-login-password | docker login --username AWS --password-stdin $ECR_URI
-docker pull $ECR_URI:$IMAGE_TAG
+# Authenticate with ECR
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin $ECR_URI
 
-# Run new container
+# Pull the latest image from ECR
+docker pull $REPO_URI:$IMAGE_TAG
+
+# Run container
 docker run -d -p 80:8000 --name $CONTAINER_NAME $ECR_URI:$IMAGE_TAG
